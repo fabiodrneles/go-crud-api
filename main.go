9880847a7faa9a99c306468ed4d7a214a007b5e0
fabiodrneles <fabiodrneles@gmail.com>
2025-09-16 20:42:08 -0300

    install mondodb and created createTask()

--- main.go
+++ main.go
@@ -1,13 +1,71 @@
 package main
 
 import (
+	"context"
+	"go-crud-api/models"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var collection *mongo.Collection
+
+func connectDB() {
+	// 1. Cria um contexto com timeout para a operação de conexão
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// 2. Conecta ao MongoDB usando o contexto
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal("Failed to create client:", err) // Usa log.Fatal para parar a aplicação se não puder criar o cliente
+	}
+
+	// 3. PING no banco de dados para verificar se a conexão está realmente ativa
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("Failed to connect to MongoDB:", err) // Para a aplicação se não conseguir conectar
+	}
+
+	collection = client.Database("taskdb").Collection("tasks")
+	log.Println("✅ Connected to MongoDB!")
+}
+
+func createTask(c *gin.Context) {
+	var task models.Task
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	task.CreatedAt = time.Now()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	result, err := collection.InsertOne(ctx, task)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"id": result.InsertedID})
+
+}
+
 func main() {
+	connectDB()
 	router := gin.Default()
+
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "ola mundo!!"})
 	})
+
+	router.POST("/tasks", createTask)
+
 	router.Run(":8080")
 }
